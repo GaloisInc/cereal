79a81f485d9248120e3185aab5159cd1e8dead02
Trevor Elliott <trevor@galois.com>
2015-04-08 15:03:55 -0700

    Modernize the cabal file, as we only build back to 7.6.1

--- src/Data/Serialize.hs
+++ src/Data/Serialize.hs
@@ -2,13 +2,11 @@
 {-# LANGUAGE FlexibleContexts  #-}
 {-# LANGUAGE CPP #-}
 
-#ifdef GENERICS
 {-# LANGUAGE DefaultSignatures
            , TypeOperators
            , BangPatterns
            , KindSignatures
            , ScopedTypeVariables #-}
-#endif
 
 -----------------------------------------------------------------------------
 -- |
@@ -63,10 +61,8 @@ import qualified Data.Ratio           as R
 import qualified Data.Tree            as T
 import qualified Data.Sequence        as Seq
 
-#ifdef GENERICS
 import GHC.Generics
 import Control.Applicative ((*>),(<*>),(<$>),pure)
-#endif
 
 ------------------------------------------------------------------------
 
@@ -84,13 +80,11 @@ class Serialize t where
     -- | Decode a value in the Get monad
     get :: Get t
 
-#ifdef GENERICS
     default put :: (Generic t, GSerialize (Rep t)) => Putter t
     put = gPut . from
 
     default get :: (Generic t, GSerialize (Rep t)) => Get t
     get = to <$> gGet
-#endif
 
 ------------------------------------------------------------------------
 -- Wrappers to run the underlying monad
@@ -487,7 +481,6 @@ instance (Serialize i, Ix i, Serialize e, IArray UArray e)
     put = putIArrayOf put put
     get = getIArrayOf get get
 
-#ifdef GENERICS
 ------------------------------------------------------------------------
 -- Generic Serialze
 
@@ -610,4 +603,3 @@ instance (SumSize a, SumSize b) => SumSize (a :+: b) where
 
 instance SumSize (C1 c a) where
     sumSize = Tagged 1
-#endif
