9a0a5e90a3ad4c736593c16fed82ed75c729464c
Trevor Elliott <trevor@galois.com>
2015-06-28 08:52:57 -0700

    Try not to rebuild the sandbox for the benchmark

--- bench/build.sh
+++ bench/build.sh
@@ -2,9 +2,12 @@
 
 set -e
 
-cabal sandbox init
-cabal sandbox add-source ../
-cabal install --only-dep
-cabal configure
+if [ ! -f cabal.sandbox.config ]; then
+	cabal sandbox init
+	cabal sandbox add-source ../
+	cabal install --only-dep
+	cabal configure
+fi
+
 cabal build
 ./dist/build/cereal-bench/cereal-bench
