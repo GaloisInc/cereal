53d92ad33ff4f30d17360a459114a219675be0fd
Ryan Scott <ryan.gl.scott@gmail.com>
2016-11-07 19:13:46 -0500

    Remove redundant `do`

--- src/Data/Serialize/Put.hs
+++ src/Data/Serialize/Put.hs
@@ -457,8 +457,7 @@ lazyToStrictByteString = packChunks
 
 -- | Pack the chunks of a lazy bytestring into a single strict bytestring.
 packChunks :: L.ByteString -> S.ByteString
-packChunks lbs = do
-    S.unsafeCreate (fromIntegral $ L.length lbs) (copyChunks lbs)
+packChunks lbs = S.unsafeCreate (fromIntegral $ L.length lbs) (copyChunks lbs)
   where
     copyChunks !L.Empty                         !_pf = return ()
     copyChunks !(L.Chunk (S.PS fpbuf o l) lbs') !pf  = do
